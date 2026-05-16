1a9e9bc17725ee132937333ea4dc27afe310cc7b
Sujay Awale <sujayawale125@gmail.com>
2026-05-16 21:27:49 +0530

    Space removed

--- src/main/java/com/example/DeliveryServiceApplication/DeliveryServiceApplication.java
+++ src/main/java/com/example/DeliveryServiceApplication/DeliveryServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DeliveryServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliveryServiceApplication.class, args); 
+		SpringApplication.run(DeliveryServiceApplication.class, args);
 	}
 
 }
